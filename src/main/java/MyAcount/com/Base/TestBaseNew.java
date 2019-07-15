c9cb033246283ea825e59bc58df132e4222c1e9f
abhinav-ireslab <abhinavgarg3@gmail.com>
2019-07-15 11:32:59 +0530

    LatestCode2

--- src/main/java/MyAcount/com/Base/TestBaseNew.java
+++ src/main/java/MyAcount/com/Base/TestBaseNew.java
@@ -1,4 +1,5 @@
 package MyAcount.com.Base;
+
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -19,74 +20,98 @@ import MyAccount.com.Util.TestUtil;
 
 import java.util.*;
 
-
 public class TestBaseNew {
-	
-		public static WebDriver driver;
-		public static Properties prop;
 
-		
-		public TestBaseNew() throws IOException{
-		try {	
-		    prop = new Properties();
-			FileInputStream ip = new FileInputStream("C:\\Users\\E2E\\eclipse-workspace\\AutomationE2E\\src\\main\\java\\MyAccount\\com\\Config\\config.properties");
-			prop.load(ip);
-			String browserName = prop.getProperty("browser");
-			System.out.println("Browser Name: "+browserName);
-		}
-		catch(FileNotFoundException e) {
-		e.printStackTrace(); 		
-	     }
-		catch(IOException e) {
-		e.printStackTrace();
-	     }
+	public static WebDriver driver;
+	public static Properties prop;
 
-		
-	}
-		@BeforeTest
-		public static void inititalization() {
-			//System.out.println("Intialization");
-			String browserName = prop.getProperty("browser");
-			
-			String os = System.getProperty("os.name").toLowerCase();
-			if (os.contains("win")){
-    //Operating system is based on Windows
-				System.setProperty("webdriver.gecko.driver","C:\\Users\\E2E\\eclipse-workspace\\AutomationE2E\\Drivers\\geckodriver.exe");
-				driver = (WebDriver) new FirefoxDriver();
+	public TestBaseNew() throws IOException {
+		try {
+			prop = new Properties();
+
+			try {
+				FileInputStream ip = new FileInputStream(
+						"C:\\Users\\E2E\\git\\New folder\\testing\\src\\main\\java\\MyAccount\\com\\Config\\config.properties");
+				prop.load(ip);
+
+			} catch (Exception e) {
+				FileInputStream ip = new FileInputStream("C:\\Users\\E2E\\eclipse-workspace\\"
+						+ "AutomationE2E\\src\\main\\java\\MyAccount\\com\\Config\\config.properties");
+				prop.load(ip);
+
 			}
-			//else if (os.contains("osx")){
-    //Operating system is Apple OSX based
-			//}      
-			else if (os.contains("nix") || os.contains("aix") || os.contains("nux")){
-    //Operating system is based on Linux/Unix/*AIX
-				System.setProperty("webdriver.gecko.driver","C://Users//E2E//eclipse-workspace//AutomationE2E//Drivers//geckodriver.exe");
+
+			String browserName = prop.getProperty("browser");
+			System.out.println("Browser Name: " + browserName);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@BeforeTest
+	public static void inititalization() {
+		// System.out.println("Intialization");
+		String browserName = prop.getProperty("browser");
+
+		String os = System.getProperty("os.name").toLowerCase();
+		if (os.contains("win")) {
+			// Operating system is based on Windows
+			try {
+
+				System.setProperty("webdriver.gecko.driver",
+						"C:\\Users\\E2E\\git\\New folder\\testing\\Drivers\\geckodriver.exe");
 				driver = (WebDriver) new FirefoxDriver();
-			
-			//System.out.println("Browser Name: "+browserName);
-			/*if(browserName.equals("Firefox")) {
-				//System.out.println("Browser Name: "+browserName);
-				System.setProperty("webdriver.gecko.driver","C:\\Users\\E2E\\eclipse-workspace\\AutomationE2E\\Drivers\\geckodriver.exe");
-			driver = (WebDriver) new FirefoxDriver();
-			//System.out.println("Launch Firefox");
-			}else if (browserName.equals("chrome")){
-				System.setProperty("webdriver.chrome.driver","C:\\Users\\E2E\\eclipse-workspace\\AutomationE2E\\Drivers\\chromedrive.exe");
-				driver = (WebDriver) new ChromeDriver();
-*/			}
-		
-		//driver.manage().window().maximize();
-		Dimension d = new Dimension(1550,800); 
-		//Resize the current window to the given dimension
-		driver.manage().window().setSize(d); 
+
+			} catch (Exception e) {
+				System.setProperty("webdriver.gecko.driver",
+						"C:\\Users\\E2E\\eclipse-workspace\\AutomationE2E\\Drivers\\geckodriver.exe");
+				driver = (WebDriver) new FirefoxDriver();
+
+			}
+		}
+		// else if (os.contains("osx")){
+		// Operating system is Apple OSX based
+		// }
+		else if (os.contains("nix") || os.contains("aix") || os.contains("nux")) {
+			// Operating system is based on Linux/Unix/*AIX
+			try {
+
+				System.setProperty("webdriver.gecko.driver",
+						"C:\\Users\\E2E\\git\\New folder\\testing\\Drivers\\geckodriver.exe");
+				driver = (WebDriver) new FirefoxDriver();
+
+			} catch (Exception e) {
+				System.setProperty("webdriver.gecko.driver",
+						"C:\\Users\\E2E\\eclipse-workspace\\AutomationE2E\\Drivers\\geckodriver.exe");
+				driver = (WebDriver) new FirefoxDriver();
+
+			}
+
+			// System.out.println("Browser Name: "+browserName);
+			/*
+			 * if(browserName.equals("Firefox")) {
+			 * //System.out.println("Browser Name: "+browserName);
+			 * System.setProperty("webdriver.gecko.driver",
+			 * "C:\\Users\\E2E\\eclipse-workspace\\AutomationE2E\\Drivers\\geckodriver.exe")
+			 * ; driver = (WebDriver) new FirefoxDriver();
+			 * //System.out.println("Launch Firefox"); }else if
+			 * (browserName.equals("chrome")){ System.setProperty("webdriver.chrome.driver",
+			 * "C:\\Users\\E2E\\eclipse-workspace\\AutomationE2E\\Drivers\\chromedrive.exe")
+			 * ; driver = (WebDriver) new ChromeDriver();
+			 */ }
+
+		// driver.manage().window().maximize();
+		Dimension d = new Dimension(1550, 800);
+		// Resize the current window to the given dimension
+		driver.manage().window().setSize(d);
 		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS); 
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
 		driver.get(prop.getProperty("url"));
-		
-		
-		}
-		
-		
+
 	}
 
-
-
+}
--- src/test/java/MyAccount/com/Test/SSLTest.java
+++ src/test/java/MyAccount/com/Test/SSLTest.java
@@ -1,7 +1,11 @@
 package MyAccount.com.Test;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.maven.surefire.shade.org.apache.maven.shared.utils.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -34,6 +38,10 @@ public class SSLTest extends TestBaseNew{
 		}
 		@Test
 		public void sslfillformtest() throws InterruptedException, IOException {
+			
+			try {
+				
+			
 			//Thread.sleep(20000);
 			SSL = new SSLPage();
 			SSL.sslfillform();
@@ -41,4 +49,15 @@ public class SSLTest extends TestBaseNew{
 			SSL.nextbutton1();
 			
 		}
+		
+		catch(Exception e)
+		{
+
+			File scrFile = (((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE));
+			FileUtils.copyFile(scrFile,
+					new File("C:\\Users\\E2E\\Pictures\\Screen\\Images" + System.currentTimeMillis() + ".png"));
+
+		}
+		
 }
+		}
